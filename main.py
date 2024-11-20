aa11975f38dc9fa11fe5f2d858a2828e54b25827
slepeylukee08 <159123907+slepeylukee08@users.noreply.github.com>
2024-11-20 10:29:01 +0000

    final_Tichy

--- main.py
+++ main.py
@@ -6,16 +6,26 @@ random_array = [random.randint(0, 100) for _ in range(10)]
 
 print(random_array) #Vypise nahodne cisla v nahodnem poradi v poli
 
-#2 - Vytvořte jednoduchý sort.
+#Úkol 2 - Vytvořte jednoduchý sort.
 
 # Vytvoření jednorozměrného pole s 10 náhodnými hodnotami mezi 0 a 100
-random_array = [random.randint(0, 100) for _ in range(10)]
+array = [random.randint(0, 100) for _ in range(10)]
+print("Všechny vypsané pole, každý krok po sobě")
 
-# Bubble Sort - jednoduché třídění
-for i in range(len(random_array)):    #Cyklus bude probíhat tolikrát, kolik je prvků v poli random_array, promenna i bude nabyvat hodnotu od 0 do delky pole -1
-        if random_array[j] > random_array[j + 1]: #Cyklus prochází polem od indexu 0 až do indexu len(random_array) - i - 1
-            random_array[j], random_array[j + 1] = random_array[j + 1], random_array[j] #Tohle znamená že dochází k promeně nebo výměně hodnot na určené pozicích j a j + 1 v poli random_array
+import random
 
-# Výpis výsledků
-print("Náhodné pole:", random_array)
+def bubble_sort():
+    n = len(array)
+    for i in range(n-1):
+        for j in range(0,n-i-1):
+            if array[j] > array[j+1] :
+                array[j], array[j+1] = array[j+1], array[j] # Prohození prvků
+            print(array) # Výpis aktuálního stavu pole
+    return array
+
+print(bubble_sort())
+
+
+
+
 
